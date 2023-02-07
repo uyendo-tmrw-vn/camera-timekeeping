c4ebd1a397271b20f2af374b395a70f5e04fa8ae
Uyen Do TMRW-VN <99628467+uyendo-tmrw-vn@users.noreply.github.com>
2023-02-07 13:36:42 +0700

    adsf

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-        <WebcamCapture1 />
-        {/* <WebcamCapture /> */}
+        {/* <WebcamCapture1 /> */}
+        <WebcamCapture />
         {/* <WebcamDeviceId /> */}
       </main>
     </>
